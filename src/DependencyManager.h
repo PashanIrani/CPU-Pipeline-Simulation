810350ba56118bfd22d14d7d1d3258fca1478417
Manav <manav09meghpara@gmail.com>
2022-04-14 10:37:31 -0700

    fixed issue

--- src/DependencyManager.h
+++ src/DependencyManager.h
@@ -7,10 +7,15 @@
 class DependencyManager {
   // A HashMap keeps track of which dependencies have been completed 
   std::unordered_map<std::string, bool> instStatus;
+  std::unordered_map<std::string, bool> duplicate;
 
   // gets status of an instruction. If id is an empty string, it returns true as an empty string would be equal to "is no instruction been completed"
   bool getStatus(std::string id) {
-    return id.compare("") == 0 ? true : instStatus[id];
+    if (instStatus.find(id) != instStatus.end()){ // if id is in instStatus give status
+      return id.compare("") == 0 ? true : instStatus[id];
+    }
+    return id.compare("") == 0 ? true : duplicate[id];
+    
   }
 
   // sets status of an instruction
@@ -18,21 +23,37 @@ class DependencyManager {
     instStatus[id] = value;
   }
   
+  // sets status of an instruction
+  void setDuplicate(std::string id, bool value) {
+    duplicate[id] = value;
+  }
+
   public:
   DependencyManager() {
     instStatus = {};
+    duplicate = {};
   }
 
   // Add instruction to hashmap, with default status being false
   void add(Instruction * inst) {
     if (inst == NULL) return;
-    set(inst->id, false);
+    if (instStatus.count(inst->id)==0){
+      set(inst->id, false);
+      return;
+    }
+    setDuplicate(inst->id, false);
+    return;
   } 
 
   // Mark an instruction to be complete
   void markComplete(Instruction * inst) {
     if (inst == NULL) return;
-    set(inst->id, true);
+    if (instStatus.find(inst->id) != instStatus.end()){
+      set(inst->id, true);
+      return;
+    }
+    setDuplicate(inst->id, true);
+    return;
   }
 
   // Print HashMaps contents
